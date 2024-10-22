b3b8c6167a90429e70a2dcfacc00822ecaf06b9d
Melissa Ruiz <melruizglez97@gmail.com>
2024-10-22 20:00:31 +0100

    feat(construcciones): se avanza creando conexion rol

--- construccion/app/Contracts/UsuarioContract.php
+++ construccion/app/Contracts/UsuarioContract.php
@@ -3,9 +3,12 @@
 namespace App\Contracts;
 
 final class RolContract {
-    public const TABLE_NAME = "roles";
+    public const TABLE_NAME = "usuarios";
     public const COL_ID = "id";
     public const COL_NOMBRE = "nombre";
+    public const COL_PASSWORD = "password";
+    public const COL_ROL_ID = "rol_id";
+
 
 
     private function __construct() {}
---
+++ construccion/app/Models/Rol.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Models;
+
+class Rol {
+    private int $id;
+    private string $nombre;
+
+
+
+    /**
+     * Get the value of nombre
+     *
+     * @return string
+     */
+    public function getNombre(): string
+    {
+        return $this->nombre;
+    }
+
+    /**
+     * Set the value of nombre
+     *
+     * @param string $nombre
+     *
+     * @return self
+     */
+    public function setNombre(string $nombre): self
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of id
+     *
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set the value of id
+     *
+     * @param int $id
+     *
+     * @return self
+     */
+    public function setId(int $id): self
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+}
