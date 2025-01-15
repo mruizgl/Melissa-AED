ccf9ac61119701a20252a4b790a6a64c8de335d4
Melissa Ruiz <melruizglez97@gmail.com>
2025-01-15 19:46:00 +0000

    feat(ejemplos-apirest-java): se añaden ejemplos

---
+++ JAVA-APIREST/institutov2/src/main/java/es/iespuerto/mr/institutov2/config/TasksConfig.java
@@ -0,0 +1,10 @@
+package es.iespuerto.mr.institutov2.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+public class TasksConfig {
+
+}
