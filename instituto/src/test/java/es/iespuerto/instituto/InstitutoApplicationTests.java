8f72fef9aacf8c8bd102226239cac7d1bf9ff2e1
Melissa Ruiz <125923092+mruizgl@users.noreply.github.com>
2025-01-27 09:53:52 +0000

    chore: reestructuracion de carpetas

---
+++ instituto/src/test/java/es/iespuerto/instituto/InstitutoApplicationTests.java
@@ -0,0 +1,13 @@
+package es.iespuerto.instituto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstitutoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
